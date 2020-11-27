ee3d34e6b450f2d81124c91a7b2e6784bd465a50
sunjinsen <sunjinsen@shouqianba.com>
2020-11-27 10:34:18 +0800

    update

--- week01/week01.py
+++ week01/week01.py
@@ -1,21 +1,27 @@
 from datetime import datetime
+from pathlib import Path
 import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-handler = logging.FileHandler(datetime.now().strftime("%Y-%m-%d") + ".log")
-formatter = logging.Formatter("%(message)s")
-handler.setFormatter(formatter)
-handler.setLevel(logging.INFO)
-
-logger.addHandler(handler)
+import os
 
 
 def func():
+    # 获取当前的时间
     timestamp = datetime.now()
 
-    logger.info("调用函数的时间为：{}".format(timestamp))
+    # 判断当前工作路径下是否存在目录，若不存在则新建
+    log_file_parent_path = os.path.join(os.getcwd(), datetime.now().strftime("%Y-%m-%d"))
+
+    p = Path(log_file_parent_path)
+
+    if not p.is_dir():
+        p.mkdir(exist_ok=True, parents=True)
+
+    # 日志记录
+    logging.basicConfig(filename=log_file_parent_path + "/geek.log",
+                        level=logging.INFO,
+                        format="%(message)s")
+
+    logging.info("调用函数的时间为：{}".format(timestamp))
 
 
 if __name__ == "__main__":
